699a7146532d372527a7a97401f4bfad9f2695dc
Waranko <mari.waranko@gmail.com>
2021-06-27 17:12:50 +0300

    Add test blocks

--- src/test/java/com/epam/rd/autotasks/arrays/Tests.java
+++ src/test/java/com/epam/rd/autotasks/arrays/Tests.java
@@ -11,61 +11,81 @@ public class Tests {
 
     @Test
     public void sumTest(){
-        int[] array = new int[]{1, 3, 2, 8, 15, 199};
-        int actual = SumOfEvenNumbers.sum(array);
 
-        assertEquals(18, actual);
+        int[] array;
+        int actual;
 
-        array = new int[]{1, 3, 2, 8, 199, 15};
-        actual = SumOfEvenNumbers.sum(array);
+        {
+            array = new int[]{1, 3, 2, 8, 15, 199};
+            actual = SumOfEvenNumbers.sum(array);
 
-        assertEquals(202, actual);
-
-        array = new int[10000];
-        Arrays.fill(array, 1);
-        actual = SumOfEvenNumbers.sum(array);
-
-        assertEquals(5000, actual);
-
-        array = new int[700];
-        actual = SumOfEvenNumbers.sum(array);
-
-        assertEquals(0, actual);
-
-        array = new int[1000];
-        Arrays.fill(array, 1);
-        array[0] = 11;
-        array[1] = -100;
-        actual = SumOfEvenNumbers.sum(array);
-
-        assertEquals(510, actual);
-
-        array = new int[10000];
-        Arrays.fill(array, 1);
-        array[15] = 2;
-        array[1432] = -100;
-        actual = SumOfEvenNumbers.sum(array);
-
-        assertEquals(4899, actual);
-
-        array = new int[1001];
-        Arrays.fill(array, -100);
-        for (int i = 0; i < array.length; i+=2){
-            array[i] = 10;
+            assertEquals(18, actual);
         }
-        actual = SumOfEvenNumbers.sum(array);
 
-        assertEquals(5010, actual);
+        {
+            array = new int[]{1, 3, 2, 8, 199, 15};
+            actual = SumOfEvenNumbers.sum(array);
 
-        array = new int[1001];
-        Arrays.fill(array, -100);
-        for (int i = 0; i < array.length; i+=2){
-            array[i] = 10;
+            assertEquals(202, actual);
         }
-        array[14] = 200;
-        actual = SumOfEvenNumbers.sum(array);
 
-        assertEquals(5200, actual);
+        {
+            array = new int[10000];
+            Arrays.fill(array, 1);
+            actual = SumOfEvenNumbers.sum(array);
+
+            assertEquals(5000, actual);
+        }
+
+        {
+            array = new int[700];
+            actual = SumOfEvenNumbers.sum(array);
+
+            assertEquals(0, actual);
+        }
+
+        {
+            array = new int[1000];
+            Arrays.fill(array, 1);
+            array[0] = 11;
+            array[1] = -100;
+            actual = SumOfEvenNumbers.sum(array);
+
+            assertEquals(510, actual);
+        }
+
+        {
+            array = new int[10000];
+            Arrays.fill(array, 1);
+            array[15] = 2;
+            array[1432] = -100;
+            actual = SumOfEvenNumbers.sum(array);
+
+            assertEquals(4899, actual);
+        }
+
+        {
+            array = new int[1001];
+            Arrays.fill(array, -100);
+            for (int i = 0; i < array.length; i+=2){
+                array[i] = 10;
+            }
+            actual = SumOfEvenNumbers.sum(array);
+
+            assertEquals(5010, actual);
+        }
+
+        {
+            array = new int[1001];
+            Arrays.fill(array, -100);
+            for (int i = 0; i < array.length; i+=2){
+                array[i] = 10;
+            }
+            array[14] = 200;
+            actual = SumOfEvenNumbers.sum(array);
+
+            assertEquals(5200, actual);
+        }
     }
 
     @Test
